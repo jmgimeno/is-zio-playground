1a924df0f8ba660c234d6b5315f884f36959d0dd
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2023-05-04 17:01:01 +0200

    renamed some files

--- src/main/scala/variance/Covariance.scala
+++ src/main/scala/variance/Covariance.scala
@@ -7,10 +7,3 @@ object Covariance:
   trait Gen[-R, +A]:
     def sample: ZIO[R, Nothing, A]
     // def contains(a: A): ZIO[R, Nothing, Boolean]
-
-object Contravariant:
-
-  trait Drink
-
-  // trait FoodProcessor[-Ingredient]:
-  //   def process(ingredient: Ingredient): (Drink, Ingredient)
