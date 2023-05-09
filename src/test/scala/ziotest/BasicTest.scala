5a9775fb63991626a7677128f5114990446f6d60
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2023-05-09 08:26:17 +0200

    refactored tests

--- src/test/scala/ziotest/BasicTest.scala
+++ src/test/scala/ziotest/BasicTest.scala
@@ -6,6 +6,12 @@ import zio.test.Assertion.*
 
 object BasicTest extends ZIOSpecDefault:
 
-  def spec = test("simple zio succeed") {
-    assertZIO(ZIO.succeed(1 + 1))(equalTo(2))
-  }
+  val spec = suite("simple zio succeed")(
+    test("classic assertion") {
+      assertZIO(ZIO.succeed(1 + 1))(equalTo(2))
+    },
+    test("smart assertion") {
+      for result <- ZIO.succeed(1 + 1)
+      yield assertTrue(result == 2)
+    }
+  )
--- src/test/scala/ziotest/TimeSpec.scala
+++ src/test/scala/ziotest/TimeSpec.scala
@@ -1,4 +1,4 @@
-package time
+package ziotest
 
 import zio.*
 import zio.test.*
@@ -9,7 +9,7 @@ object TimeSpec extends ZIOSpecDefault:
   val goShopping: ZIO[Any, Nothing, Unit] =
     Console.printLine("Going shopping!").orDie.delay(1.hour)
 
-  def spec = suite("ExampleSpec")(
+  val spec = suite("ExampleSpec")(
     test("goShopping delays for one hour") {
       for
         fiber <- goShopping.fork
@@ -17,11 +17,25 @@ object TimeSpec extends ZIOSpecDefault:
         _ <- fiber.join
       yield assertCompletes
     },
+    test("goShopping delays for one hour (smart)") {
+      for
+        fiber <- goShopping.fork
+        _ <- TestClock.adjust(1.hour)
+        poll <- fiber.poll
+      yield assertTrue(poll == Some(Exit.succeed(())))
+    },
     test("goShopping is not finished before one hour") {
       for
         fiber <- goShopping.fork
         _ <- TestClock.adjust(59.minutes)
         poll <- fiber.poll
       yield assert(poll)(isNone)
+    },
+    test("goShopping is not finished before one hour (smart)") {
+      for
+        fiber <- goShopping.fork
+        _ <- TestClock.adjust(59.minutes)
+        poll <- fiber.poll
+      yield assertTrue(poll == None)
     }
   )
