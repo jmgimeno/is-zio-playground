2c87b9da90c8509af6e492e3665c629262b2d8b2
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2023-05-10 16:44:06 +0200

    after session on May 10th

--- src/main/scala/exitsandcauses/ExitsAndCauses.scala
+++ src/main/scala/exitsandcauses/ExitsAndCauses.scala
@@ -3,6 +3,13 @@ package exitsandcauses
 import zio.*
 import java.text.NumberFormat
 import java.io.IOException
+import zio.Cause.Empty
+import zio.Cause.Fail
+import zio.Cause.Die
+import zio.Cause.Interrupt
+import zio.Cause.Stackless
+import zio.Cause.Then
+import zio.Cause.Both
 
 object ExitsAndCauses extends ZIOAppDefault:
 
@@ -24,6 +31,50 @@ object ExitsAndCauses extends ZIOAppDefault:
   // val effect3 = effect1.unsandbox
 
   val effect4: ZIO[Any, IOException, Unit] = effect2.unsandbox
+
+  val effect5 =
+    effect1.foldCauseZIO(
+      cause =>
+        cause match
+          case Fail(error, trace) => Console.printLine(s"Failed with $error")
+          case Die(throwable, trace) =>
+            Console.printLine(s"Died with $throwable")
+          case _ => ZIO.die(new Exception("another case"))
+      ,
+      success => Console.printLine(s"Success with $success")
+    )
+
+    effect1.sandbox.foldZIO(
+      cause =>
+        cause match
+          case Fail(error, trace) => Console.printLine(s"Failed with $error")
+          case Die(throwable, trace) =>
+            Console.printLine(s"Died with $throwable")
+          case _ => ZIO.die(new Exception("another case"))
+      ,
+      success => Console.printLine(s"Success with $success")
+    )
+
+    effect1.foldZIO(
+      ioEx => Console.printLine(s"Died with $ioEx"),
+      success => Console.printLine(s"Success with $success")
+    )
+
+  val getANumber =
+    for
+      input <- Console.readLine("Enter a number: ").orDie
+      result <- ZIO
+        .attempt(360 / input.toInt)
+        .refineToOrDie[NumberFormatException]
+    yield result
+
+    val getANumber2 =
+      (for
+        input <- Console.readLine("Enter a number: ")
+        result <- ZIO
+          .attempt(360 / input.toInt)
+      yield result)
+        .refineToOrDie[NumberFormatException]
 
   val run =
     for
