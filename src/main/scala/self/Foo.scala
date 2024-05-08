22da38bf006a724b1e8a05e11551eca9f47dcc72
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2024-05-08 15:01:58 +0200

    updated example for self

--- src/main/scala/self/Foo.scala
+++ src/main/scala/self/Foo.scala
@@ -2,9 +2,14 @@ package self
 
 // Alias for this, specially useful when using Inner classes
 
-class Foo:
+class Foo {
   self =>
-  class Bar:
-    def getFoo: Foo = self
+  val x = 1
+  new AnyRef {
+    val x = 2
+    println(this.x) // 2
+    println(self.x) // 1
+  }
+}
 
 // Self-types are much more complicated https://docs.scala-lang.org/tour/self-types.html
